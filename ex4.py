bdd47381b6c7cb4ba88821951b020631d3e28917
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-11 21:17:10 +0300

    Changed name of x to matrix

--- ex4.py
+++ ex4.py
@@ -6,9 +6,9 @@ def main() -> None:
     create a 10x10 matrix,
     in which the elements on the borders will be equal to 1, and inside 0
     """
-    x = np.ones((10, 10))
-    x[1:-1, 1:-1] = 0
-    print(x)
+    matrix = np.ones((10, 10))
+    matrix[1:-1, 1:-1] = 0
+    print(matrix)
 
 
 if __name__ == '__main__':
