98a076ee1144a394db65e6d907a3989ec5b222e5
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-11 20:24:47 +0300

    Finished task ex3

--- ex3.py
+++ ex3.py
@@ -1,3 +1,25 @@
+import numpy as np
+import nptyping
+
+
+def find_row_col(matrix: nptyping.ndarray) -> tuple[int, int]:
+    """
+    Find the number of rows and columns of a given matrix.
+    :param matrix: Matrix to find her shapes.
+    :return: tuple of row and col
+    """
+    return matrix.shape
+
+
+def main() -> None:
+    """
+    Find the number of rows and columns of a given matrix.
+    """
+    matrix = np.arange(1, 10).reshape((3, 3))
+
+    row, col = find_row_col(matrix)
+    print(row, col)
+
 
 if __name__ == '__main__':
-    pass
+    main()
