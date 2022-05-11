ebfd85c5444542f4e24f1c266988836eaf08ad47
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-11 20:49:20 +0300

    Finished task 4

--- ex4.py
+++ ex4.py
@@ -1,3 +1,15 @@
+import numpy as np
+
+
+def main() -> None:
+    """
+    create a 10x10 matrix,
+    in which the elements on the borders will be equal to 1, and inside 0
+    """
+    x = np.ones((10, 10))
+    x[1:-1, 1:-1] = 0
+    print(x)
+
 
 if __name__ == '__main__':
-    pass
+    main()
